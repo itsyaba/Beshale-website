f99b0cd2e2ebf6384d0212ac2aba830a6c5f9208
Yaba <reallegendlyrics@gmail.com>
2022-04-05 20:43:35 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,81 @@
+const hamburger = document.querySelector('.hamburger')
+const close_btn = document.querySelector('.close-btn')
+const nav_links = document.querySelector('.nav-links')
+const up_arrow = document.querySelector('.up-arrow')
+const nav_two = document.querySelector('.nav-two')
+
+
+hamburger.addEventListener('click', () => {
+      nav_links.classList.toggle('smooth')
+})
+
+close_btn.addEventListener('click', () => {
+      nav_links.classList.remove('smooth')
+})
+
+window.addEventListener('scroll', function () {
+      up_arrow.classList.toggle("active-for-back", window.scrollY > 350);
+      nav_two.classList.toggle("active-for-fixed", window.scrollY > 700);
+//   console.log(window.scrollY)
+})
+
+
+
+
+// 'use strict'
+// var testim = document.getElementById("testim"),
+//     testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+//     testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+//     testimleftArrow = document.getElementById("left-arrow"),
+//     testimRightArrow = document.getElementById("right-arrow"),
+//     testimSpeed = 4500,
+//     currentSlide = 0,
+//     currentActive = 0,
+//     testimTimer
+//     ;
+// // coding with nick
+// window.onload = function () {
+
+//     // Testim Script
+//     function playSlide(slide) {
+//         for (var k = 0; k < testimDots.length; k++) {
+//             testimContent[k].classList.remove("active");
+//             testimContent[k].classList.remove("inactive");
+//             testimDots[k].classList.remove("active");
+//         }
+//         if (slide < 0) {
+//             slide = currentSlide = testimContent.length - 1;
+//         }
+//         if (slide > testimContent.length - 1) {
+//             slide = currentSlide = 0;
+//         }
+//         if (currentActive != currentSlide) {
+//             testimContent[currentActive].classList.add("inactive");
+//         }
+//         testimContent[slide].classList.add("active");
+//         testimDots[slide].classList.add("active");
+
+//         currentActive = currentSlide;
+
+//         clearTimeout(testimTimer);
+//         testimTimer = setTimeout(function () {
+//             playSlide(currentSlide += 1);
+//         }, testimSpeed)
+//     }
+// // coding with nick
+//     testimleftArrow.addEventListener("click", function () {
+//         playSlide(currentSlide -= 1);
+//     })
+//     testimRightArrow.addEventListener("click", function () {
+//         playSlide(currentSlide += 1);
+//     })
+
+//     for (var l = 0; l < testimDots.length; l++) {
+//         testimDots[l].addEventListener("click", function () {
+//             playSlide(currentSlide = testimDots.indexOf(this));
+//         })
+//     }
+//     playSlide(currentSlide);
+
+// }
+
---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(() => {
+    // navbar
+    $('.navbar-show-btn').click(() => {
+        $('.navbar-collapse').addClass('showNavbar');
+    });
+
+    $('.navbar-hide-btn').click(() => {
+        $('.navbar-collapse').removeClass('showNavbar');
+    });
+
+    // slick slider
+    $('.hero-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        dots: true,
+        speed: 600,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+
+    // stopping transition
+    let resizeTimer;
+    $(window).on('resize', () => {
+        $(document.body).addClass('resize-transition-stopper');
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(() => {
+            $(document.body).removeClass('resize-transition-stopper');
+        }, 400);
+    });
+});
+
+console.log(Boolean("false"))
+
