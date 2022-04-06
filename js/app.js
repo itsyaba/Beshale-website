11be3ce9cbb40acb2dabd6f4d0032d78be1221b3
Yaba <reallegendlyrics@gmail.com>
2022-04-06 13:25:55 +0300

    second commit

--- js/app.js
+++ js/app.js
@@ -3,7 +3,19 @@ const close_btn = document.querySelector('.close-btn')
 const nav_links = document.querySelector('.nav-links')
 const up_arrow = document.querySelector('.up-arrow')
 const nav_two = document.querySelector('.nav-two')
+const show = document.querySelectorAll('.show h2')
+const coll = document.querySelectorAll('.coll')
+const drop_btn = document.querySelectorAll('.fa-angle-down')
 
+
+for (let i = 0; i < show.length; i++) {
+      let show_place = coll[i]
+      show[i].addEventListener('click', () => {
+            show_place.classList.toggle('show')
+            drop_btn[i].classList.toggle('down')
+      })
+      
+}
 
 hamburger.addEventListener('click', () => {
       nav_links.classList.toggle('smooth')
@@ -18,7 +30,6 @@ window.addEventListener('scroll', function () {
       nav_two.classList.toggle("active-for-fixed", window.scrollY > 700);
 //   console.log(window.scrollY)
 })
-
 
 
 
